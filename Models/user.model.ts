517e336c23c3fe5cc1acddedab99de617b1c0b9e
muhammad-abdullah11 <abdullahworld111@gmail.com>
2026-04-10 01:05:20 +0500

    make a usermodel also auto hashing password using bcryptjs

---
+++ Models/user.model.ts
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: [true, "FullName is required"],
+        trim: true
+    },
+    userName: {
+        type: String,
+        required: [true, "UserName is required"],
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"]
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    VerifyOtp: {
+        type: String,
+        default: null
+    },
+    VerifyOtpExpire: {
+        type: Date,
+        default: null
+    }
+},{ timestamps: true });
+
+
+
+userSchema.pre("save", async function () {
+    
+  if (!this.isModified("password")) return;
+
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+export default User;
