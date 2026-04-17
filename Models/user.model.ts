59531cffb451470dc2edcb4475face6b1baa14f2
muhammad-abdullah11 <abdullahworld111@gmail.com>
2026-04-17 20:13:55 +0500

    refector : add new entity -type- in userModel -userORadmin

--- Models/user.model.ts
+++ Models/user.model.ts
@@ -17,6 +17,11 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: [true, "Password is required"]
     },
+    type: {
+        type: String,
+        enum: ["user", "admin"],
+        default: "user"
+    },
     isVerified: {
         type: Boolean,
         default: false
@@ -29,15 +34,15 @@ const userSchema = new mongoose.Schema({
         type: Date,
         default: null
     }
-},{ timestamps: true });
+}, { timestamps: true });
 
 
 
 userSchema.pre("save", async function () {
-    
-  if (!this.isModified("password")) return;
 
-  this.password = await bcrypt.hash(this.password, 10);
+    if (!this.isModified("password")) return;
+
+    this.password = await bcrypt.hash(this.password, 10);
 });
 
 const User = mongoose.models.User || mongoose.model("User", userSchema);
