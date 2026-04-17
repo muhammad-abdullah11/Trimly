9c385d23e9e13656d18bec7aa2c825b1c1ae3394
muhammad-abdullah11 <abdullahworld111@gmail.com>
2026-04-17 16:51:18 +0500

    feat: handle the delete url logic

--- components/Urls.tsx
+++ components/Urls.tsx
@@ -20,13 +20,18 @@ const Urls = () => {
         fetchUrls();
     }, []);
 
+    const deleteUrl = async (id: string) => {
+        const res = await axios.delete(`/api/url/${id}`);
+        setUrls(res.data.urls);
+    };
+
     return (
         <main className="flex-1">
             <section className="container mx-auto px-4">
                 <div className="flex flex-col gap-4">
                     <h1 className="text-4xl font-bold">Your URLs</h1>
                     <div className="flex flex-col gap-4">
-                        <h2 className="text-2xl font-semibold">Total URLs: {urls.length}</h2>
+                        <h2 className="text-2xl font-semibold">Total URLs: {urls?.length ? urls.length : 0}</h2>
                     </div>
                     <section className="grid py-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                         {urls.map((url) => (
@@ -44,6 +49,7 @@ const Urls = () => {
                                     <h2 className="text-lg">{url.clicks}</h2>
                                 </div>
                                 <button onClick={() => copyToClipboard(url.shortUrl)} className="px-4 py-2 border border-gray-300 rounded-lg cursor-pointer focus:outline-none focus:ring-2 focus:ring-emerald-500 hover:bg-emerald-500 hover:text-white transition-colors duration-200 ">Copy</button>
+                                <button onClick={() => deleteUrl(url._id)} className="px-4 py-2 rounded-lg cursor-pointer bg-red-500 hover:bg-red-700 text-white transition-colors duration-200 ">Delete</button>
 
                             </div>
                         ))}
