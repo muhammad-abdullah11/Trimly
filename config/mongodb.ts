3a1469d426893cccafa90a98ce95a3b8399f4ad2
muhammad-abdullah11 <abdullahworld111@gmail.com>
2026-04-17 21:59:15 +0500

    chore(db): update environment variable for MongoDB connection

--- config/mongodb.ts
+++ config/mongodb.ts
@@ -3,21 +3,21 @@ import { NextResponse } from 'next/server';
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const  DbConnect = async () => {
-    const mongoUrl = process.env.LOCAL_HOST_MONGO_URL;
-    if(!mongoUrl){
+export const DbConnect = async () => {
+    const mongoUrl = process.env.MONGO_URL;
+    if (!mongoUrl) {
         throw new Error("MongoDb Url are required")
     };
 
     try {
         const alreadyConnected = mongoose.connection.readyState === 1;
-        if(alreadyConnected){
+        if (alreadyConnected) {
             console.log("Already Connected to MongoDB")
             return;
         };
         await mongoose.connect(mongoUrl);
         console.log("Connected to MongoDB")
-    } catch (error :any) {
+    } catch (error: any) {
         console.log(error)
         return NextResponse.json({ message: "Error connecting to MongoDB: " + error.message }, { status: 500 });
     }
