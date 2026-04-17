35a516ad8ca7096b5d1bbc03b2a47b7977512eca
muhammad-abdullah11 <abdullahworld111@gmail.com>
2026-04-17 21:17:42 +0500

    refector : add a validation for admin to redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
 import HeroSection from "@/components/HeroSection";
 import Urls from "@/components/Urls";
+import { getServerSession } from "next-auth";
+import nextAuthOptions from "@/config/nextAuth";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export const metadata = {
+  title: "Trimly - URL Shortener",
+  description: "Trimly is a free URL shortener that helps you shorten your links and track your clicks.",
+};
+
+export default async function Home() {
+  const session = await getServerSession(nextAuthOptions);
+  if (session?.user.type == "admin") {
+    redirect("/admin");
+  }
   return (
     <>
       <HeroSection />
